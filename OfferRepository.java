619468dcfa9732530981adc1851473cde28fb187
Bhavadeepreddy <84385855+Bhavadeepreddy@users.noreply.github.com>
2022-10-17 22:45:25 +0530

    Delete OfferRepository.java

--- OfferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.casestudy4.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.casestudy4.bean.Offer;
-
-public interface OfferRepository extends JpaRepository<Offer, Long> {
-
-}
